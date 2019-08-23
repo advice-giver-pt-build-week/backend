2e7d63a3203df606754575985a80306a2d9b4ca5
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-08-22 20:19:49 -0600

    fixed id's for the seed users.

--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,30 @@
-const db = require('../database/dbConfig.js');
-
+const db = require( "../database/dbConfig.js" );
 
 module.exports = {
-    findAll,
-    findById,
-    add,
-    update,
-    remove,
-    findUser
+  findAll, findById, add, update, remove, findUser
+};
+
+function findUser( username ){
+  return db( "users" ).where( { username } ).first();
 }
 
-function findUser(username) {
-    return db('users').where({ username }).first();
-};
+function add( user ){
+  return db( "users" ).insert( user, ['id', 'username', 'email', 'adviceGiver', 'expertise', 'yearsOfExperience', 'age'] );
+}
 
-function add(user) {
-    return db('users').insert(user);
-};
+async function update( id, changes ){
+  await db( "users" ).where( { id } ).update( changes );
+  return db( "users" ).where( { id } ).first();
+}
 
-async function update(id, changes) {
-    await db('users').where({ id }).update(changes);
-    return db('users').where({ id }).first();
-};
+function remove( id ){
+  return db( "users" ).where( { id } ).del();
+}
 
-function remove(id) {
-    return db('users').where({ id }).del();
-};
+function findById( id ){
+  return db( "users" ).where( { id } ).first();
+}
 
-function findById(id) {
-    return db('users').where({ id }).first();
-};
-
-function findAll() {
-    return db('users')
-};
+function findAll(){
+  return db( "users" );
+}
